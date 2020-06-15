2d19b49c0e5c172a4cf1739e6c35d7cc54a3efa3
RishabhKodes <rishabhbhandari6@gmail.com>
2020-06-15 11:08:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+var cons = require('consolidate');
+const fs = require('fs') 
+
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.get('/', function(req, res, next) {
+    res.render('medium1');
+});
+
+export function output(data){
+fs.writeFile('output.json', data, (err) => {  
+    if (err) throw err; 
+    })
+}
+
+app.listen(3000, function() {
+    console.log('Express server listening on port 3000');
+});
+
+module.exports = app;
+
